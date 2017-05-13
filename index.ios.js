ebf8a0b3c5ea383dcdaa9652a522e8eee5714b38
Nivedita Mittal <nivedita.mittal@gmail.com>
2017-05-13 11:27:27 -0400

    basic styles set up

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   AppRegistry,
   StyleSheet,
@@ -15,39 +16,61 @@ import {
 export default class whatsappCloneReact extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+      <View style={styles.leftHeaderContainer}>
+      <Text style={styles.logoText}>WhatsApp</Text>
+      </View>
+      <View style={styles.rightHeaderContainer}>
+      <Icon name='search' style={styles.rightHeaderIcon} />
+      <Icon name='call' style={styles.rightHeaderIcon} />
+      <Icon name='more-vert' style={styles.rightHeaderIcon} />
+      </View>
+      </View>
+      <View style={styles.contentContainer}>
+      </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    height: 24
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  headerContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#075e54",
+    alignItems: "center",
+    paddingRight: 5
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  leftHeaderContainer: {
+    alignItems: "flex-start",
+    flexDirection: "row"
   },
+  rightHeaderContainer: {
+    alignItems: "flex-end",
+    flexDirection: "row"
+  },
+  contentContainer: {
+    flex: 6,
+  },
+  logoText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    alignItems: "flex-start",
+    marginLeft: 10
+  },
+  rightHeaderIcon: {
+    color: '#fff',
+    fontSize: 23,
+    padding: 5
+  }
 });
 
 AppRegistry.registerComponent('whatsappCloneReact', () => whatsappCloneReact);

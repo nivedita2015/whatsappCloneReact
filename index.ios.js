aa7f60c6c0f93cc3021508f0fce327c6c0458641
Nivedita Mittal <nivedita.mittal@gmail.com>
2017-05-13 12:22:58 -0400

    first page done

--- index.ios.js
+++ index.ios.js
@@ -10,10 +10,35 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  Image
 } from 'react-native';
 
+import { fetch } from 'fetch';
+
+const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 != r1})
+
 export default class whatsappCloneReact extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      randomUsersDataSource : ds.cloneWithRows([]),
+      loaded: false
+    }
+  }
+
+componentDidMount() {
+  fetch('https://gist.githubusercontent.com/nivedita2015/10e76ced192e4309aef7159d9e65d73c/raw/7a812e3176c19b0087e55d91f5717a200a69d1a8/randomUsers.json')
+  .then(response => response.json())
+  .then((data) => {
+    this.setState({
+      randomUsersDataSource: ds.cloneWithRows(data),
+      loaded: true
+    })
+  });
+}
+
   render() {
     return (
       <View style={styles.mainContainer}>
@@ -28,9 +53,37 @@ export default class whatsappCloneReact extends Component {
       </View>
       </View>
       <View style={styles.contentContainer}>
+      <ListView
+        initialListSize={5}
+        enableEmptySection={true}
+        dataSource={this.state.randomUsersDataSource}
+        renderRow={(person) => { return this.renderUserRow(person)}} />
       </View>
       </View>
     );
+  }
+renderUserRow(person){
+    return (
+      <View style = {styles.listItemContainer}>
+    <View style= {styles.iconContainer}>
+     <Image source={{uri:person.image}} style={styles.initStyle} resizeMode='contain' />
+    </View>
+    <View style = {styles.callerDetailsContainer}>
+     <View style={styles.callerDetailsContainerWrap}>
+      <View style={styles.nameContainer}>
+        <Text>{person.first_name}</Text>
+        <View style={styles.dateContainer}>
+          <Icon name={person.missed ? "call-missed" : "call-received"} size={15} color={person.missed ? "#ed788b" : "#075e54"} />
+          <Text style={{ fontWeight:'400', color:'#666', fontSize:12 }}>{person.date} {person.time}</Text>
+        </View>
+       </View>
+     <View style={styles.callIconContainer}>
+      <Icon name="phone" color='#075e54' size={23} style={{ padding:5 }} />
+     </View>
+    </View>
+   </View>
+  </View>
+    )
   }
 }
 
@@ -70,6 +123,45 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 23,
     padding: 5
+  },
+  listItemContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: "flex-start"
+  },
+  callerDetailsContainer: {
+    flex: 4,
+    justifyContent: "center",
+    borderBottomColor: "rgba(92,94,94,0.5)",
+    borderBottomWidth: 0.25
+  },
+  callerDetailsContainerWrap: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  nameContainer: {
+    alignItems: "flex-start",
+    flex: 1
+  },
+  dateContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  callIconContainer: {
+    flex: 1,
+    alignItems: "flex-end"
+  },
+  initStyle: {
+    borderRadius: 30,
+    width: 60,
+    height: 60
   }
 });
 
